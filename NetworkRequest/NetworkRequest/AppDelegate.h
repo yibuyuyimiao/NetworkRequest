5d2e3e050f5b44169b7b38d631bcccb912a05a4f
Your Name <you@example.com>
2017-05-13 09:08:51 +0800

    新建项目

---
+++ NetworkRequest/NetworkRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkRequest
+//
+//  Created by 原 on 2017/5/13.
+//  Copyright © 2017年 ybym.training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetworkRequest/NetworkRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkRequest
+//
+//  Created by 原 on 2017/5/13.
+//  Copyright © 2017年 ybym.training. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
